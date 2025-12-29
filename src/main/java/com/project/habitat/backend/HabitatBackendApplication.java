6e9955d829cfd52f418b26f0c431ca050da36ec8
kushvlshresthv <kushvlshresthv@gmail.com>
2025-12-29 17:21:06 +0545

    Big Bang

---
+++ src/main/java/com/project/habitat/backend/HabitatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.habitat.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/habitat/backend/HabitatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.habitat.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
